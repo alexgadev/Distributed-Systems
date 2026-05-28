b20071da63eec0b12030cd465798f2200351c6f0
Alex Garcia Amezcua <alexgadev@hotmail.com>
2026-05-28 20:32:10 +0200

    refactor/8-redis-filter working

--- task 1/redis/insult_filter.py
+++ task 1/redis/insult_filter.py
@@ -1,28 +1,22 @@
 import redis
 
-r = redis.Redis(decode_responses=True)
-pubsub = r.pubsub()
 
-INPUT = "insult_broadcast"
-OUTPUT = "insult_filtered"
-
-INSULTS = ["idiot", "stupid", "nerd"]
-
-def submit_text(text):
-    for insult in INSULTS:
-        text = text.replace(insult, "CENSORED")
-    r.rpush("filtered", text)
-    return text
-
-def get_filtered():
-    pass
+INSULT_QUEUE = "insult_queue"
+FILTERED_LIST_KEY = "insult_filtered"
 
 if __name__ == "__main__":
-    pubsub.subscribe(INPUT)
-    print("Filter listenning...")
+    r = redis.Redis(decode_responses=True)
+    
 
-    for message in pubsub.listen():
-        if message["type"] == "message":
-            insult = message["data"]
-            filtered = submit_text(insult)
-            r.publish(OUTPUT, filtered)
+    print("Redis InsultFilter listening...")
+    try:
+        while True:
+            INSULTS = r.lrange("insults", 0, -1)
+            _, text = r.blpop(INSULT_QUEUE) # blocks until a job arrives
+            for insult in INSULTS:
+                text = text.replace(insult, "CENSORED")
+
+            # store filtered text into the result list
+            r.rpush(FILTERED_LIST_KEY, text)
+    except KeyboardInterrupt:
+        print("Closing Redis InsultFilter...")
