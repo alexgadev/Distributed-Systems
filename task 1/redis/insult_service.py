1773abe02ea7f7b343993893f59c171e1ee03245
Alex Garcia Amezcua <alexgadev@hotmail.com>
2026-05-28 11:58:31 +0200

    enhancement/wrapped InsultService into a class to ease working with the broadcaster

--- task 1/redis/insult_service.py
+++ task 1/redis/insult_service.py
@@ -4,33 +4,36 @@ import random
 
 from multiprocessing import Process
 
-r = redis.Redis(decode_responses=True)
-
 INSULT_LIST_KEY = "insults"
 CHANNEL = "insult_broadcast"
 
-def add_insult(insult):
-    if insult not in r.lrange(INSULT_LIST_KEY, 0, -1):
-        r.rpush(INSULT_LIST_KEY, insult)
-        return True
-    else:
-        return False
+class InsultService:
+    def __init__(self) -> None:
+        self.r = redis.Redis(decode_responses=True)
 
-def get_insults():
-    return r.lrange(INSULT_LIST_KEY, 0, -1)
+    def add_insult(self, insult):
+        if insult not in self.r.lrange(INSULT_LIST_KEY, 0, -1):
+            self.r.rpush(INSULT_LIST_KEY, insult)
+            return True
+        else:
+            return False
 
-def start_broadcast():
-    while True:
-        n_subscribers = r.pubsub_numsub(CHANNEL).get(CHANNEL, 0)
-        if n_subscribers > 0:
-            insults = get_insults()
-            if insults:
-                r.publish(CHANNEL, random.choice(insults))
-        time.sleep(5)
+    def get_insults(self):
+        return self.r.lrange(INSULT_LIST_KEY, 0, -1)
+
+    def start_broadcast(self):
+        while True:
+            n_subscribers = self.r.pubsub_numsub(CHANNEL).get(CHANNEL, 0)
+            if n_subscribers > 0:
+                insults = self.get_insults()
+                if insults:
+                    self.r.publish(CHANNEL, random.choice(insults))
+            time.sleep(5)
 
 if __name__ == "__main__":
     print("Redis InsultService running...")
-    p = Process(target=start_broadcast)
+    service = InsultService()
+    p = Process(target=service.start_broadcast)
     p.start()
     try:
         while True:
